df52a954e39d25df82a013f63f939e3df5e03b2e
Aidan Potteiger <gamesitfo@gmail.com>
2019-11-25 10:28:25 -0600

    Contours Working Woot Woot

--- visionTest/frcColor.cpp
+++ visionTest/frcColor.cpp
@@ -1,4 +1,4 @@
-#include "opencv2/imgproc.hpp"
+﻿#include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/videoio.hpp"
 #include <iostream>
@@ -6,59 +6,79 @@
 
 using namespace cv;
 using namespace std;
+int trackBars() {
 
+
+
+}
 int main() {
 
 	int thresh = 0;
 	VideoCapture cam;
-	Mat frame, frameHSV, frameRange, frameDone;
-	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3), Point(-1, -1));
+	Mat frame, frameHSV, frameRange, frameDone, frameProc;
+	vector<vector<Point> > contours;
+	Mat kernel = getStructuringElement(MORPH_CROSS, Size(5, 5), Point(-1, -1));
+	cam.set(CAP_PROP_AUTO_EXPOSURE, false);
+
 	cam.open(0);
 
+	//Int Definitions
 	int lowH = 0;
 	int lowS = 0;
 	int lowV = 0;
-	int highH = 0;
-	int highS = 0;
-	int highV = 0;
-	int maxArea = 0;
+	int highH = 100;
+	int highS = 100;
+	int highV = 100;
+	int static maxAreaSet = 10000;
+	int maxArea = (maxAreaSet / 2);
 	int minArea = 0;
+	int exposure = 5;
 
+	//Trackbars
+	cv::namedWindow("Sliders");
+	cv::namedWindow("Contour Sliders");
+	cv::createTrackbar("lowH", "Sliders", &lowH, 255);
+	cv::createTrackbar("highH", "Sliders", &highH, 255);
+	cv::createTrackbar("lowS", "Sliders", &lowS, 255);
+	cv::createTrackbar("highS", "Sliders", &highS, 255);
+	cv::createTrackbar("lowV", "Sliders", &lowV, 255);
+	cv::createTrackbar("highV", "Sliders", &highV, 255);
+	cv::createTrackbar("Exposure", "Sliders", &exposure, 10);
+	cv::createTrackbar("minArea", "Contour Sliders", &minArea, (maxAreaSet - 1));
+	cv::createTrackbar("maxArea", "Contour Sliders", &maxArea, maxAreaSet);
 
+	//Forever loop
 	for (;;) {
+		cam.set(CAP_PROP_EXPOSURE, exposure);
 		cam >> frame;
-
+		cv::imshow("Sad :(", frame);
 		if (frame.empty()) {
 			cout << "failed to open img.jpg" << endl;
 		}
 
 		cvtColor(frame, frameHSV, COLOR_BGR2HSV);
 		inRange(frameHSV, Scalar(lowH, lowS, lowV), Scalar(highH, highS, highV), frameRange);
-		morphologyEx(frameRange, frameDone, MORPH_OPEN, kernel, Point(-1, -1), 1);
-		
-		Mat canny_output;
-		Canny(frameRange, canny_output, thresh, thresh * 2);
+		morphologyEx(frameRange, frameDone, MORPH_CLOSE, kernel, Point(-1, -1), 1);
+		morphologyEx(frameDone, frameDone, MORPH_OPEN, kernel, Point(-1, -1), 1);
 
-		vector<vector<Point> > contours;
-		vector<Vec4i> hierarchy;
+		findContours(frameDone, contours, RETR_LIST, CHAIN_APPROX_SIMPLE, Point(0, 0));
+		frameProc = frameDone;
+		cvtColor(frameProc, frameProc, COLOR_GRAY2RGB);
 
-		Mat draw = Mat::zeros(canny_output.size(), CV_8UC3);
-
-		findContours(frameDone, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
+		//All contour stuff
 		for (size_t i = 0; i < contours.size(); i++)
 		{
-			drawContours(draw, contours, i, Scalar(0, 0, 255), 3, 8, hierarchy, 0, Point());
+
+			if (contourArea(contours[i]) < minArea || contourArea(contours[i]) > maxArea) {
+				continue;
+			}
+			Rect boundRect = boundingRect(contours[i]);
+			float ratio = contourArea(contours[i]) / (boundRect.width * boundRect.height);
+			drawContours(frameProc, contours, i, Scalar(0, 0, 255), 3);
 		}
 		
-		namedWindow("Sliders");
-		createTrackbar("lowH", "Sliders", &lowH, 255);
-		createTrackbar("highH", "Sliders", &highH, 255);
-		createTrackbar("lowS", "Sliders", &lowS, 255);
-		createTrackbar("highS", "Sliders", &highS, 255);
-		createTrackbar("lowV", "Sliders", &lowV, 255);
-		createTrackbar("highV", "Sliders", &highV, 255);
-		imshow("Hello :)", frameDone);
-		imshow("Drawings", draw);
+
+		cv::imshow("Hello :)", frameProc);
 		char keypress = waitKey(10);
 		if (keypress == 'q' || keypress == 27) break;
 
